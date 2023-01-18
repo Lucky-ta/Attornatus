84a723d593a2a5785fb222d2efe8237de8a3a6d5
Lucas Maieski <lucmaieski@gmail.com>
2023-01-18 20:52:16 -0300

    Create user put router

--- src/main/java/br/com/lucas/attornatus/controller/ClientController.java
+++ src/main/java/br/com/lucas/attornatus/controller/ClientController.java
@@ -1,9 +1,12 @@
 package br.com.lucas.attornatus.controller;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,4 +29,15 @@ public class ClientController {
         return clientRepository.save(user);
     }
 
+    @PutMapping("/user/{id}")
+    public Client updateClient(@PathVariable Long id, @RequestBody Client client) {
+        Client existingClient = clientRepository.findById(id).get();
+        if (client.getName() != null) {
+            existingClient.setName(client.getName());
+        }
+        if (client.getBirthdate() != null) {
+            existingClient.setBirthdate(client.getBirthdate());
+        }
+        return clientRepository.save(existingClient);
+    }
 }
